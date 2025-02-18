516b8f8d2014838c8e7b598d59a31484fba38d62
jangsei <wod5438@naver.com>
2025-02-18 17:25:51 +0900

    수정

--- src/method/quiz/Quiz3.java
+++ src/method/quiz/Quiz3.java
@@ -5,20 +5,20 @@ public class Quiz3 {
 	// 두 수를 비교하는 함수를 만드시오
 	// 둘 중 더 큰 숫자를 구하시오
 	// ex) 10, 5 => 10
-	public static void method(int n1, int n2) {
-		int i = (n1 > n2) ? n1 : n2;
-		System.out.println(i);
-	}
-	
-	// return x
-	// 조건문 사용
-	public static void method1(int n1, int n2) {
-		if(n1 > n2) { // 앞의 숫자가 더 크면
-			System.out.println(n1); // 앞의 숫자 출력
-		} else {
-			System.out.println(n2); // 그렇지 않으면 뒤의 숫자를 출력
-		}
-	}
+//	public static void method(int n1, int n2) {
+//		int i = (n1 > n2) ? n1 : n2;
+//		System.out.println(i);
+//	}
+//	
+//	// return x
+//	// 조건문 사용
+//	public static void method1(int n1, int n2) {
+//		if(n1 > n2) { // 앞의 숫자가 더 크면
+//			System.out.println(n1); // 앞의 숫자 출력
+//		} else {
+//			System.out.println(n2); // 그렇지 않으면 뒤의 숫자를 출력
+//		}
+//	}
 	
 	public static int method2(int n1, int n2) {
 		if(n1 > n2) { // 앞의 숫자가 더 크면
@@ -30,7 +30,10 @@ public class Quiz3 {
 	
 	public static void main(String[] args) {
 		
-		method(100, 200);
+//		method(100, 200);
+		
+		int result = method2(100,200);
+		System.out.println(result);
 
 	}
 
